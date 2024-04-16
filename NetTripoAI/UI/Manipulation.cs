4e964af1217a4d1b6f14a5ffc9b104a37c987237
jorgemagic <jferrero@plainconcepts.com>
2024-04-16 17:45:11 +0200

    - Removed objects

--- NetTripoAI/UI/Manipulation.cs
+++ NetTripoAI/UI/Manipulation.cs
@@ -1,16 +1,23 @@
 ﻿using Evergine.Bindings.Imgui;
 using Evergine.Bindings.Imguizmo;
+using Evergine.Common.Input.Keyboard;
+using Evergine.Common.Input;
 using Evergine.Framework;
 using Evergine.Framework.Graphics;
 using Evergine.Framework.Physics3D;
 using Evergine.Mathematics;
 using Evergine.UI;
 using System;
+using Evergine.Framework.Services;
 
 namespace NetTripoAI.UI
 {
     public unsafe class Manipulation : Behavior
     {
+        [BindService]
+        protected GraphicsPresenter graphicsPresenter;
+
+        private Entity selectedEntity = null;
         private Transform3D transform = null;
 
         private Matrix4x4 view;
@@ -25,13 +32,13 @@ namespace NetTripoAI.UI
         }
 
         protected override void Update(TimeSpan gameTime)
-        {
+        {            
             // Selected element
             var camera = this.Managers.RenderManager?.ActiveCamera3D;
 
             var mouse = camera.Display?.MouseDispatcher;
             if (mouse != null)
-            {
+            {                
                 var btn = mouse.ReadButtonState(Evergine.Common.Input.Mouse.MouseButtons.Left);
                 if (btn == Evergine.Common.Input.ButtonState.Pressing)
                 {
@@ -42,7 +49,7 @@ namespace NetTripoAI.UI
                     var hitResult = this.Managers.PhysicManager3D.RayCast(ref ray, 100);
                     if (hitResult.Succeeded)
                     {
-                        var selectedEntity = hitResult.PhysicBody.BodyComponent.Owner;
+                        this.selectedEntity = hitResult.PhysicBody.BodyComponent.Owner;                        
                         this.transform = selectedEntity.FindComponent<Transform3D>();
                     }
                     else
@@ -55,6 +62,14 @@ namespace NetTripoAI.UI
                 // Show Manipulator
                 if (this.transform != null)
                 {
+                    // Keyboard
+                    KeyboardDispatcher keyboardDispatcher = this.graphicsPresenter.FocusedDisplay?.KeyboardDispatcher;
+
+                    if (keyboardDispatcher?.ReadKeyState(Keys.Delete) == ButtonState.Pressing)
+                    {
+                        this.Managers.EntityManager.Remove(this.selectedEntity);        
+                    }
+
                     var io = ImguiNative.igGetIO();
                     ImguizmoNative.ImGuizmo_SetRect(0, 0, io->DisplaySize.X, io->DisplaySize.Y);
 
